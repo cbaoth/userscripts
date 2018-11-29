5c633ccf47e584e346b4c51ae2cb229300aabaf6
cbaoth <dev@cbaoth.de>
2018-11-29 14:08:26 +0100

    Update amazon price add line break before shipping details

--- amazon-links.user.js
+++ amazon-links.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name        Amazon ASIN Links (Preis端berwachung)
+// @name        Amazon ASIN Links (Preisüberwachung)
 // @name:en     Amazon ASIN Links (price tracking)
 // @namespace   https://cbaoth.de
 // @version     0.3
 // @downloadURL https://github.com/cbaoth/userscripts/raw/master/amazon-links.user.js
-// @description F端gt auf allen Amazon Produktseiten links zur Preis端berwachungsseite keepa.com sowie ein direkter Amazon Produktlink (ohne unn端tze URL Parameter, zum Teilen des Links).
+// @description Fügt auf allen Amazon Produktseiten links zur Preisüberwachungsseite keepa.com sowie ein direkter Amazon Produktlink (ohne unnütze URL Parameter, zum Teilen des Links).
 // @description:en Adds links to the price watching page keepa.com as well as a direct link to the Amazon Product (without any URL parameters, for sharing).
 //
 // @include     /^https?://[^/]*amazon.\w+//
@@ -27,7 +27,7 @@ const PRICE_SELECTOR = 'tr#priceblock_ourprice_row > td:last-child, #priceblock_
 
 // add links
 function addAmazonLinks() {
-    if (! $('input#ASIN:first').length) {
+    if (!$('input#ASIN:first').length) {
         return; // this doesn't seem to be a product page
     }
 
@@ -36,10 +36,10 @@ function addAmazonLinks() {
 
     // get top level domain (the simple way)
     var tld = document.domain.split('.').pop();
-    if ([ 'au', 'br', 'mx' ].indexOf(tld) > -1) { // add .com to some domains
-        tld = 'com.'+tld;
-    } else if ([ 'uk', 'jp' ].indexOf(tld) > -1) { // add .co to others
-        tld = 'co.'+tld;
+    if (['au', 'br', 'mx'].indexOf(tld) > -1) { // add .com to some domains
+        tld = 'com.' + tld;
+    } else if (['uk', 'jp'].indexOf(tld) > -1) { // add .co to others
+        tld = 'co.' + tld;
     }
 
     // create all new links
@@ -47,24 +47,28 @@ function addAmazonLinks() {
     // direct link
     var link1tooltip = (tld == 'de' ? 'Direkter Produktlink' : 'Direct product link');
     var link1url = 'http://amazon.' + tld + '/dp/' + asin;
-    var link1 = (SHOW_LINK_ICON ? '<img src="http://www.amazon.'+tld+'/favicon.ico" border="0" align="absmiddle" width="16" height="16" />&nbsp;' : '')
-                + '<a target="_blank" href="' + link1url + '" style="color: #e47911;' + LINK_STYLE + '" title="' + link1tooltip + '">'
-                + (SHOW_LINK_TEXT ? (tld == 'de' ? 'Direkter Link' : 'Direct Link') : '')
-                + '</a>';
+    var link1 = (SHOW_LINK_ICON ? '<img src="http://www.amazon.' + tld + '/favicon.ico" border="0" align="absmiddle" width="16" height="16" />&nbsp;' : '')
+        + '<a target="_blank" href="' + link1url + '" style="color: #e47911;' + LINK_STYLE + '" title="' + link1tooltip + '">'
+        + (SHOW_LINK_TEXT ? (tld == 'de' ? 'Direkter Link' : 'Direct Link') : '')
+        + '</a>';
 
     // keepa.com
-    var keepaIds = { "com":1, "uk":2, "de":3, "fr":4, "jp":5, "ca":6, "cn":7, "it":8, "es":9, "in":10, "mx":11, "br":12, "au":13 };
+    var keepaIds = { "com": 1, "uk": 2, "de": 3, "fr": 4, "jp": 5, "ca": 6, "cn": 7, "it": 8, "es": 9, "in": 10, "mx": 11, "br": 12, "au": 13 };
     var link2tooltip = (tld == 'de' ? 'Keepa Preishistorie' : 'Keepa price history');
     var link2url = 'https://keepa.com/#!product/' + keepaIds[tld] + '-' + asin;
     var link2 = (SHOW_LINK_ICON ? '<img src="https://keepa.com/favicon.ico" border="0" align="absmiddle" width="16" height="16" />&nbsp;' : '')
-                + '<a target="_blank" href="' + link2url + '" style="color: #039;' + LINK_STYLE + '" title="' + link2tooltip + '">'
-                + (SHOW_LINK_TEXT ? 'Keepa' : '') + '</a>';
+        + '<a target="_blank" href="' + link2url + '" style="color: #039;' + LINK_STYLE + '" title="' + link2tooltip + '">'
+        + (SHOW_LINK_TEXT ? 'Keepa' : '') + '</a>';
 
-    // add the links as new table row below the price information
-    //$('table.product > tbody:last > tr:last, table.a-lineitem > tbody:last > tr:last')
-    //  .after('<tr><td></td><td>'+link1+link2+'</td></tr>');
-    $(PRICE_SELECTOR)
-        .append('<span style="margin-left: 2em;">'+link1+' &nbsp; '+link2+'</span>');
+    // add the links next to the price information
+    var td = $(PRICE_SELECTOR);
+    // if there are additional shipping details next to the price, add a line break and instert links before next to the price
+    var shipSpan = td.find('#ourprice_shippingmessage');
+    if (shipSpan) {
+        shipSpan.prepend('<span style="margin-left: 2em;">' + link1 + ' &nbsp; ' + link2 + '</span><br/>');
+    } else {
+        td.append('<span style="margin-left: 2em;">' + link1 + ' &nbsp; ' + link2 + '</span>');
+    }
 }
 
 waitForKeyElements(PRICE_SELECTOR, addAmazonLinks);
