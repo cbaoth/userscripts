9f23baf1bb0edaa4ad783a62d637851ae412eda5
Andreas Weyer <a.weyer@seeburger.de>
2018-11-30 10:37:07 +0100

    Add jenkins blue ocean support

--- jenkins-tweaks.user.js
+++ jenkins-tweaks.user.js
@@ -25,18 +25,20 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 
 /* Search and replace + highlight the given patterns in the jenkins job console output
  */
-const CONSOLE_SUBSTITUTION = [['pre.console-output',
-    [// test progress
-        [/(\(0 Failed for now\))/g, "$1", { "color": "cornflowerblue" }],
-        [/(\([1-9][0-9]* Failed for now\))/g, "$1", { "color": "red" }],
-        // errors, warnings, exceptions, ignoring positive test progress "(0 failed"
-        [/(?<=^|[ \[\n\r])(error|(<!\(0 )fail(?:ure)|abort(?:ed)?)(?=[ \]\n\r!.]|$)/gi, "$1", { "color": "red", "font-weight": "bold" }],
-        [/(?<=^|[ .\n\r])(\w*(?:exception|error|Caused by:))(?=[ :\n\r!.]|$)/gi, "$1", { "color": "red", "font-weight": "bold" }],
-        [/(?<=^|[ \[\n\r])(warn(?:ing)?|unstable|skip(?:ed|ping)?)(?=[ \]\n\r!.]|$)/gi, "$1", { "color": "#FFC000", "font-weight": "bold" }],
-        // unit test summary
-        [/((?:errors|failures): [1-9][0-9]*)/gi, "$1", { "color": "red", "font-weight": "bold" }],
-        [/((?:skipped): [1-9][0-9]*)/gi, "$1", { "color": "#FFC000", "font-weight": "bold" }],
-        [/(?<=^| )(success)(?=[ \n\r]|$)/gi, "$1", { "color": "limegreen", "font-weight": "bold" }]]
+const SEL_CONSOLE = "pre.console-output"; // job console selector
+const SEL_CONSOLE_BO = "div.test-console, div.log-body"; // blue ocean console selectors
+const CONSOLE_SUBSTITUTION = [[SEL_CONSOLE + ", " + SEL_CONSOLE_BO,
+[// test progress
+    [/(\(0 Failed for now\))/g, "$1", { "color": "cornflowerblue" }],
+    [/(\([1-9][0-9]* Failed for now\))/g, "$1", { "color": "red" }],
+    // errors, warnings, exceptions, ignoring positive test progress "(0 failed"
+    [/(?<=^|[ \[\n\r])(error|(<!\(0 )fail(?:ure)|abort(?:ed)?)(?=[ \]\n\r!.]|$)/gi, "$1", { "color": "red", "font-weight": "bold" }],
+    [/(?<=^|[ .\n\r])(\w*(?:exception|error|Caused by:))(?=[ :\n\r!.]|$)/gi, "$1", { "color": "red", "font-weight": "bold" }],
+    [/(?<=^|[ \[\n\r])(warn(?:ing)?|unstable|skip(?:ed|ping)?)(?=[ \]\n\r!.]|$)/gi, "$1", { "color": "#FFC000", "font-weight": "bold" }],
+    // unit test summary
+    [/((?:errors|failures): [1-9][0-9]*)/gi, "$1", { "color": "red", "font-weight": "bold" }],
+    [/((?:skipped): [1-9][0-9]*)/gi, "$1", { "color": "#FFC000", "font-weight": "bold" }],
+    [/(?<=^| )(success)(?=[ \n\r]|$)/gi, "$1", { "color": "limegreen", "font-weight": "bold" }]]
 ]];
 
 /* {{{ -- UTILITIES ------------------------------------------------------- */
@@ -64,37 +66,44 @@ function cbFindTextNodes(e) {
 
 /* }}} -- CONSOLE --------------------------------------------------------- */
 // job console output
-if (/\/console(Full)?(\/|$)/.test(window.location.pathname)) {
-    function logSobsitutions() {
-        // -- TEXT SUBSTITUTION --------------------------------------------------
-        for (var tsIdxSel in CONSOLE_SUBSTITUTION) {//debugger; // loop: selectors
-            var tsSelector = CONSOLE_SUBSTITUTION[tsIdxSel][0];
-            jQuery.each($(tsSelector), function (i, e) {//debugger; // loop: selected elements
-                for (var tsIdxSub in CONSOLE_SUBSTITUTION[tsIdxSel][1]) {//debugger; // loop: substitutions
-                    var tsPattern = CONSOLE_SUBSTITUTION[tsIdxSel][1][tsIdxSub][0];
-                    // check if the text of the whole selection matches (limitation, but more efficient)
-                    if (!tsPattern.test($(e).text())) {
-                        continue; // no match
-                    }
-                    // text content does match
-                    var tsSubstitution = CONSOLE_SUBSTITUTION[tsIdxSel][1][tsIdxSub][1];
-                    var tsCSS = CONSOLE_SUBSTITUTION[tsIdxSel][1][tsIdxSub][2];
-                    var tsSpanClass = "optsnode_" + (tsSelector + tsPattern).cbHashCode();
-                    var tsSubSpan = '<span class="' + tsSpanClass + '">' + tsSubstitution + '</span>';
-                    // replace text node with substitution wrapped in span
-                    cbFindTextNodes($(e)).each(function (it, et) {
-                        // replace only if parent is not already span with same id, e.g. from earlier execution
-                        // we don't want to add additional spans with every repeated execution
-                        if (!$(et).parent().hasClass(tsSpanClass)) {
-                            $(et).replaceWith($(et).text().replace(tsPattern, tsSubSpan));
-                        }
-                    });
-                    //GM_addStyle('span.' + tsSpanClass + ... tsCSS ...); // TODO maybe add global style
-                    $(e).find("." + tsSpanClass).css(tsCSS);
+function subsituteText(e, substitutions) {
+    // -- TEXT SUBSTITUTION --------------------------------------------------
+    for (var tsIdxSel in substitutions) {//debugger; // loop: selectors
+        var tsSelector = substitutions[tsIdxSel][0];
+        jQuery.each($(tsSelector), function (i, e) {//debugger; // loop: selected elements
+            for (var tsIdxSub in substitutions[tsIdxSel][1]) {//debugger; // loop: substitutions
+                var tsPattern = substitutions[tsIdxSel][1][tsIdxSub][0];
+                // check if the text of the whole selection matches (limitation, but more efficient)
+                if (!tsPattern.test($(e).text())) {
+                    continue; // no match
                 }
-            });
-        }
+                // text content does match
+                var tsSubstitution = substitutions[tsIdxSel][1][tsIdxSub][1];
+                var tsCSS = substitutions[tsIdxSel][1][tsIdxSub][2];
+                var tsSpanClass = "optsnode_" + (tsSelector + tsPattern).cbHashCode();
+                var tsSubSpan = '<span class="' + tsSpanClass + '">' + tsSubstitution + '</span>';
+                // replace text node with substitution wrapped in span
+                cbFindTextNodes($(e)).each(function (it, et) {
+                    // replace only if parent is not already span with same id, e.g. from earlier execution
+                    // we don't want to add additional spans with every repeated execution
+                    if (!$(et).parent().hasClass(tsSpanClass)) {
+                        $(et).replaceWith($(et).text().replace(tsPattern, tsSubSpan));
+                    }
+                });
+                //GM_addStyle('span.' + tsSpanClass + ... tsCSS ...); // TODO maybe add global style
+                $(e).find("." + tsSpanClass).css(tsCSS);
+            }
+        });
     }
-    waitForKeyElements("pre.console-output", _.debounce(logSobsitutions, 200));
+}
+
+// job console log
+if (/\/console(Full)?(\/|$)/.test(window.location.pathname)) {
+    waitForKeyElements(SEL_CONSOLE, _.debounce((e) => subsituteText(e, CONSOLE_SUBSTITUTION), 200));
+}
+
+// blue ocean console log and test results
+if (/\/blue\/.*\/(pipeline|tests)(\/|$)/.test(window.location.pathname)) {
+    waitForKeyElements(SEL_CONSOLE_BO, _.debounce((e) => subsituteText(e, CONSOLE_SUBSTITUTION), 200));
 }
 /* {{{ -- CONSOLE --------------------------------------------------------- */
