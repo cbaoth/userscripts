532a0c06bd7f600fced301b322228f5e0e8676e1
cbaoth <dev@cbaoth.de>
2019-01-07 16:16:26 +0100

    Update copy-url-on-hover fix media issues, add div background-image support

--- copy-url-on-hover.user.js
+++ copy-url-on-hover.user.js
@@ -5,7 +5,7 @@
 //
 // @name        Copy URL on hover
 // @description Copy link and media urls on mouse hover while alt/ctrl is pressed
-// @version     0.1.2
+// @version     0.1.3
 // @downloadURL https://github.com/cbaoth/userscripts/raw/master/copy-url-on-hover.user.js
 //
 // @include     *
@@ -13,6 +13,7 @@
 // @grant       GM_setClipboard
 //
 // @require     http://code.jquery.com/jquery-latest.min.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @require     https://raw.githubusercontent.com/jashkenas/underscore/master/underscore.js
 // @require     https://github.com/cbaoth/userscripts/raw/master/lib/cblib.js
 // ==/UserScript==
@@ -39,13 +40,24 @@ this.$ = this.jQuery = jQuery.noConflict(true);
         }
     }
 
-    // register mouse events
-    function registerEvents() {
+
+    // register globalkeydown event handling the relevant modifier keys
+    // (last one counts)
+    $(document).on("keydown", function (event) {
+        var ev = event || window.event;
+        if (ev.altKey || ev.ctrlKey) {
+            currentMod = ev.which;
+        }
+    });
+
+
+    // register mouse events on media elements
+    function registerMediaEvents(element) {
 
         // copy video / image url on hover while ctrl is pressed
-        $('video', 'img').mouseenter(function (event) {
+        $(element).mouseenter(function (event) {
             var ev = event || window.event;
-            var video = this;
+            var media = $(element);
             var lastMod = 0;
             // since keyup is not working properly for mod keys, clear currentMod
             // if mod no longer pressed (key must have been released)
@@ -57,16 +69,19 @@ this.$ = this.jQuery = jQuery.noConflict(true);
                 if (currentMod == CTRL_CODE && lastMod != CTRL_CODE) {
                     lastMod = CTRL_CODE;
                     var newValue;
-                    if ($(video).attr("data")) { // data*
-                        newValue = $(video).attr("data");
-                    } else if ($(video).attr("data-mp4")) {
-                        newValue = $(video).attr("data-mp4");
-                    } else if ($(video).attr("data-src")) {
-                        newValue = $(video).attr("data-src");
-                    } else if ($(video).attr("src")) {
-                        newValue = $(video).attr("src");
+                    if (media.is('div')) {
+                        var bg_img = media.css('background-image') || "";
+                        newValue = bg_img.replace(/^url\(['"]?([^'"]*)['"]?\)/, '$1');
+                    } else if (media.attr("data")) { // data*
+                        newValue = media.attr("data");
+                    } else if (media.attr("data-mp4")) {
+                        newValue = media.attr("data-mp4");
+                    } else if (media.attr("data-src")) {
+                        newValue = media.attr("data-src");
+                    } else if (media.attr("src")) {
+                        newValue = media.attr("src");
                     } else { // nothing found
-                        $(video).find("source").each(function () { // try to find sources
+                        media.find("source").each(function () { // try to find sources
                             var source = this;
                             if ($(source).attr("src")) {
                                 newValue = $(source).attr("src");
@@ -84,11 +99,16 @@ this.$ = this.jQuery = jQuery.noConflict(true);
         }).mouseleave(function () {
             this.keyPollIntervalId && clearInterval(this.keyPollIntervalId);
         });
+    }
+
+
+    // register mouse events on link elements
+    function registerLinkEvents(element) {
 
         // copy link url on hover while alt is pressed
-        $('a').mouseenter(function (event) {
+        $(element).mouseenter(function (event) {
             var ev = event || window.event;
-            var a = this;
+            var link = $(element);
             var lastMod = 0;
             // since keyup is not working properly for mod keys, clear currentMod
             // if mod no longer pressed (key must have been released)
@@ -100,25 +120,18 @@ this.$ = this.jQuery = jQuery.noConflict(true);
             this.keyPollIntervalId = setInterval(function () {
                 if (currentMod == ALT_CODE && lastMod != ALT_CODE) {
                     lastMod = ALT_CODE;
-                    GM_setClipboard($(a)[0].href); // get absolute url
+                    GM_setClipboard(link[0].href); // get absolute url
                     tooltip('Copied Link');
                 }
             }, POLL_RATE);
         }).mouseleave(function () {
             this.keyPollIntervalId && clearInterval(this.keyPollIntervalId);
         });
-
-
-        // globally register all keydown events of the relevant modifier keys
-        // (last one counts)
-        $(document).on("keydown", function (event) {
-            var ev = event || window.event;
-            if (ev.altKey || ev.ctrlKey) {
-                currentMod = ev.which;
-            }
-        });
     }
 
-    registerEvents();
+
+    cb.waitAndThrottle('a', (e) => registerLinkEvents(e));
+    cb.waitAndThrottle('img, video, div[style*="background-image"]',
+                       (e) => registerMediaEvents(e));
 
 }());
