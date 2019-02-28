dbf533a72a0540c9867f66a1fff3ebe5b5594e43
cbaoth <dev@cbaoth.de>
2019-02-28 20:48:19 +0100

    Update copy-url-on-hover changed mod only to mod+key (alt-c/b) to avoid accidental clipboard change

--- copy-url-on-hover.user.js
+++ copy-url-on-hover.user.js
@@ -4,8 +4,8 @@
 // @copyright   2018+, userscript@cbaoth.de
 //
 // @name        Copy URL on hover
-// @description Copy link and media urls on mouse hover while alt/ctrl is pressed
-// @version     0.1.6
+// @description Copy link / media urls on mouse-over while alt-c/-b is pressed
+// @version     0.1.7
 // @downloadURL https://github.com/cbaoth/userscripts/raw/master/copy-url-on-hover.user.js
 //
 // @include     *
@@ -26,14 +26,18 @@ this.$ = this.jQuery = jQuery.noConflict(true);
     const KCODE_SHIFT = 16;
     const KCODE_CTRL = 17;
     const KCODE_ALT = 18;
+    const KCODE_META = 224;
+    const KCODE_B = 66;
+    const KCODE_C = 67;
 
     const POLL_RATE = 25;
     const SHOW_TT = true; // show tooltip?
+
     const TT_TIMEOUT = 250; // tooltip timeout
     const TT_FADEOUT = 500; // tooltip fadeout time
 
     // global variables
-    var currentMod = 0;
+    var currentKeys = { "shift": false, "ctrl": false, "alt": false, "meta": false };
 
     // create or replace tooltip
     function tooltip(html) {
@@ -43,13 +47,76 @@ this.$ = this.jQuery = jQuery.noConflict(true);
     }
 
 
-    // register global keydown event handling the relevant modifier keys
-    $(document).on("keydown keydown", function (event) {
+    function isModKey(keyCode) {
+        return keyCode === KCODE_SHIFT || keyCode === KCODE_CTRL
+            || keyCode === KCODE_ALT || keyCode === KCODE_META;
+    }
+
+
+    // register global keydown event
+    $(document).on("keydown", function (event) {
         var ev = event || window.event;
-        if (ev.altKey || ev.ctrlKey || ev.shiftKey) {
-            currentMod = ev.which; // last mod only
+        if (ev === undefined) {
+            return;
+        }
+        if (isModKey(ev.keyCode)) {
+            updateModState(event);
+        } else {
+            currentKeys["keyCode"] = ev.keyCode;
         }
     });
+
+
+    // register global keyup event
+    $(document).on("keyup", function (event) {
+        var ev = event || window.event;
+        if (ev === undefined) {
+            return;
+        }
+        // just update mods, no matter the key pressed (keyup unreliable for mods)
+        updateModState(event);
+        if (!isModKey(ev.keyCode) && currentKeys["keyCode"] === ev.keyCode) {
+            delete currentKeys["keyCode"];
+        }
+    });
+
+
+    function updateModState(event) {
+        var ev = event || window.event;
+        if (ev === undefined) {
+            return;
+        }
+        currentKeys["shift"] = ev.shiftKey;
+        currentKeys["ctrl"] = ev.ctrlKey;
+        currentKeys["alt"] = ev.altKey;
+        currentKeys["meta"] = ev.metaKey;
+    }
+
+
+    function checkModState(event, keys) {
+        var ev = event || window.event;
+        if (ev === undefined) {
+            return false;
+        }
+
+        var shift = ev.shiftKey === (keys["shift"] || false);
+        var ctrl = ev.ctrlKey === (keys["ctrl"] || false);
+        var alt = ev.altKey === (keys["alt"] || false);
+        var meta = ev.metaKey === (keys["meta"] || false);
+
+        return shift && ctrl && alt && meta;
+    }
+
+
+    function checkKeyState(keys) {
+        var metaKey = ["shift", "ctrl", "alt", "meta"];
+        for (var i in metaKey) {
+            if (currentKeys[metaKey[i]] !== (keys[metaKey[i]] || false)) {
+                return false;
+            }
+        }
+        return currentKeys["keyCode"] === keys["keyCode"];
+    }
 
 
     function getSrc(e, dict = { 'a': ['href'] }, includeBgImg = false) {
@@ -104,24 +171,23 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 
 
     // register mouse event
-    function registerSrcEvent(element, selector, keyCode, dict, ttText = 'Copied ...') {
+    function registerSrcEvent(element, selector, keys, dict, ttText = 'Copied ...') {
         $(element).mouseenter(function (event) {
             var ev = event || window.event;
             var e = $(event.target || event.srcElement);
 
             // since keyup is not working properly for mod keys, clear currentMod
             // if mod key was released
-            // key-event will set currentMod variable if pressed again while hovering
-            var lastMod = 0;
-            if (currentMod == keyCode) {
-                if (ev.which != keyCode) { // current mod no longer pressed?
-                    currentMod = 0;
-                }
+            // key-event will set currentKeys variable if pressed again while hovering
+            var lastKeys = {};
+            if (checkModState(ev, keys)) {
+                updateModState(ev);
             }
 
             this.keyPollIntervalId = setInterval(function () {
-                if (currentMod == keyCode && lastMod != keyCode) {
-                    lastMod = keyCode;
+                // matching keys are pressed and previous event didn't match (else: already triggered)?
+                if (checkKeyState(keys) && !checkKeyState(lastKeys)) {
+                    lastKeys = currentKeys;
                     var newValue = findSrc(e, selector, dict);
                     if (newValue !== undefined && newValue != "" && newValue != "none") {
                         var url = /^[./]/.test(newValue) ? document.location.origin + '/' + newValue.replace(/^[./]+/, '') : newValue;
@@ -135,12 +201,14 @@ this.$ = this.jQuery = jQuery.noConflict(true);
         });
     }
 
+    // register elements and key bindings
     waitForKeyElements('a',
-        (e) => registerSrcEvent(e, 'a', KCODE_ALT,
+        (e) => registerSrcEvent(e, 'a', { alt: true, keyCode: KCODE_C },
             { 'a': ['href'] },
             'Link Copied'));
+
     waitForKeyElements('img, video, source',
-        (e) => registerSrcEvent(e, 'img, video', KCODE_CTRL,
+        (e) => registerSrcEvent(e, 'img, video', { alt: true, keyCode: KCODE_B },
             {
                 'img': ['src'],
                 'video': ['src', 'data', 'data-mp4', 'data-webm', 'data-src'],
@@ -154,7 +222,7 @@ this.$ = this.jQuery = jQuery.noConflict(true);
             if ($(e).css('background-image') == 'none') { // filter
                 return;
             }
-            registerSrcEvent(e, undefined, KCODE_CTRL,
+            registerSrcEvent(e, undefined, { alt: true, keyCode: KCODE_B },
                 { 'body, div, span': ['css:background-image'] }, // special case
                 'Media Source Copied');
         });
