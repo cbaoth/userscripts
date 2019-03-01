ef5acaf657abcf28431ede3692ee6ec689d0b62f
cbaoth <dev@cbaoth.de>
2019-03-01 20:32:53 +0100

    Update imdb-tweaks.user.js add accuracy warning, fix add-image size in episode list

--- imdb-tweaks.user.js
+++ imdb-tweaks.user.js
@@ -4,7 +4,7 @@
 // @copyright   2018+, userscript@cbaoth.de
 //
 // @name        IMDB Tweaks
-// @version     0.1.2
+// @version     0.1.3
 // @description Some tweaks for IMDB
 // @downloadURL https://github.com/cbaoth/userscripts/raw/master/imdb-tweaks.user.js
 //
@@ -43,8 +43,10 @@ this.$ = this.jQuery = jQuery.noConflict(true);
                 + Array.join(anchors, "&nbsp;") + '</div>')
         });
 
+
         // add season average rating
         cb.waitAndDebounce('div.eplist', () => {
+            var episodeCount = $('div.eplist .list_item').length;
             var userRatings = $('div.ipl-rating-widget > div.ipl-rating-star > span.ipl-rating-star__rating');
             var myRatings = $('label.ipl-rating-interactive__star-container div.ipl-rating-interactive__star span.ipl-rating-star__rating');
             var userRatedEpisodesCount = 0;
@@ -63,31 +65,66 @@ this.$ = this.jQuery = jQuery.noConflict(true);
                     myRatingsSum += myRating;
                 }
             }
-            var userAvgRating = userRatingsSum / userRatedEpisodesCount;
-            var myAvgRating = myRatingsSum / myRatedEpisodesCount;
-            //var header = $('div.subpage_title_block > .header');
+
             var header = $('#episode_top');
             header.wrap('<div style="display: table; padding-top: 0.5em;"></div>');
             var ratingDiv = header.parent();
             header.wrap('<span style="display: table-cell;vertical-align:top;"></span>');
-            var tSpan = '<span style="display:table-cell; vertical-align:middle;">';
-            var tDiv = '<div style="display: table-cell; padding-left: 1em;"><div style="display: table-row;">';
+
+            var tSpan = '<span style="display:table-cell; vertical-align:middle;"></span>';
+            var tDiv = '<div style="display: table-cell; padding-left: 1em;"><div style="display: table-row;"></div></div>';
             var starSvg = $('div.ipl-rating-star svg.ipl-star-icon')[0];
             var added = false;
+
+            var userAvgRating = userRatingsSum / userRatedEpisodesCount;
             if (userAvgRating > 0) {
                 var yellowStar = $(starSvg).clone();
                 yellowStar.css("fill", "#c39400");
-                ratingDiv.append(tDiv + yellowStar[0].outerHTML + tSpan
-                    + Number.parseFloat(userAvgRating).toPrecision(2)
-                    + '</span></div></div>');
+                var userAvgRatingDiv = $(tDiv);
+                var userAvgRatingSpan = $(tSpan);
+                userAvgRatingSpan.append(Number.parseFloat(userAvgRating).toPrecision(2));
+                userAvgRatingDiv.children('div').append($(yellowStar));
+                userAvgRatingDiv.children('div').append(userAvgRatingSpan);
+                if (episodeCount > userRatedEpisodesCount) {
+                    userAvgRatingDiv.attr('title', 'Inaccurate due to missing ratings');
+                    userAvgRatingSpan.after('<span style="vertical-align: super; color: red;">*</span>');
+                    /* calculate average assuming 5 for unrated episodes (imputation => bad approach)
+                    var userAvgRatingM = (userRatingsSum + (episodeCount - userRatedEpisodesCount) * 5) / episodeCount;
+                    var userAvgRatingME = $('<span style="' + tSpanStyle + '">' + '(' + Number.parseFloat(myAvgRatingM).toPrecision(2) + ')</span>');
+                    userAvgRatingME.css('padding-left', '0.4em');
+                    userAvgRatingME.css('font-style', 'italic');
+                    userAvgRatingME.css('color', 'grey');
+                    userAvgRatingME.attr('title', 'Assuming rating 5 for unrated episodes.');
+                    userAvgRatingMSpan = userAvgRatingME[0].outerHTML;
+                    */
+                }
+                ratingDiv.append(userAvgRatingDiv);
                 added = true;
             }
+
+            var myAvgRating = myRatingsSum / myRatedEpisodesCount;
             if (myAvgRating > 0) {
                 var blueStar = $(starSvg).clone();
                 blueStar.css("fill", "#4268f1");
-                ratingDiv.append(tDiv + blueStar[0].outerHTML + tSpan
-                    + Number.parseFloat(myAvgRating).toPrecision(2)
-                    + '</span></div>');
+                var myAvgRatingDiv = $(tDiv);
+                var myAvgRatingSpan = $(tSpan);
+                myAvgRatingSpan.append(Number.parseFloat(myAvgRating).toPrecision(2));
+                myAvgRatingDiv.children('div').append($(blueStar));
+                myAvgRatingDiv.children('div').append(myAvgRatingSpan);
+                if (episodeCount > myRatedEpisodesCount) {
+                    myAvgRatingDiv.attr('title', 'Inaccurate due to missing ratings');
+                    myAvgRatingSpan.after('<span style="vertical-align: super; color: red;">*</span>');
+                    /* calculate average assuming 5 for unrated episodes (imputation => bad approach)
+                    var myAvgRatingM = (myRatingsSum + (episodeCount - myRatedEpisodesCount) * 5) / episodeCount;
+                    var myAvgRatingME = $('<span style="' + tSpanStyle + '">' + '(' + Number.parseFloat(myAvgRatingM).toPrecision(2) + ')</span>');
+                    myAvgRatingME.css('padding-left', '0.4em');
+                    myAvgRatingME.css('font-style', 'italic');
+                    myAvgRatingME.css('color', 'grey');
+                    myAvgRatingME.attr('title', 'Assuming rating 5 for unrated episodes.');
+                    myAvgRatingMSpan = myAvgRatingME[0].outerHTML;
+                    */
+                }
+                ratingDiv.append(myAvgRatingDiv);
                 added = true;
             }
         });
@@ -103,6 +140,7 @@ this.$ = this.jQuery = jQuery.noConflict(true);
             if (SEASON_LIST_COMPACT_DETAILS) detailsToggle();
         });
 
+
         // toggle episode description
         var detailsHidden = false;
         var detailsToggle = function () {
@@ -112,9 +150,11 @@ this.$ = this.jQuery = jQuery.noConflict(true);
             if (detailsHidden) {
                 $('div.list_item div.hover-over-image').css({ 'width': '40%', 'height': '40%' });
                 $('div.list_item img').css({ 'width': '100%', 'height': '100%' });
+                $('span.add-image-container.episode-list').css({ 'width': '89px', 'height': '50px' });
             } else {
                 $('div.list_item div.hover-over-image').css({ 'width': '100%', 'height': '100%' });
                 $('div.list_item img').css({ 'width': '100%', 'height': '100%' })
+                $('span.add-image-container.episode-list').css({ 'width': '224px', 'height': '126px' });
             }
             $('div.item_description').toggle(!detailsHidden); // toggle description text
         };
