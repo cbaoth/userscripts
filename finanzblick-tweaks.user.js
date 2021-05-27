5bf284d8e2a18303d6ece8c40e0767661bc317fe
cbaoth <dev@cbaoth.de>
2021-05-27 10:35:57 +0200

    Update finanzblick-tweaks improvements

--- finanzblick-tweaks.user.js
+++ finanzblick-tweaks.user.js
@@ -4,7 +4,7 @@
 // @copyright   2021+, userscript@cbaoth.de
 //
 // @name        Buhl Finanzblick Tweaks
-// @version     0.1
+// @version     0.2
 // @description Some improvments to the buhl finanzblick page
 // @downloadURL https://github.com/cbaoth/userscripts/raw/master/finanzblick-tweaks.user.js
 //
@@ -14,25 +14,20 @@
 //
 // @require     http://code.jquery.com/jquery-latest.min.js
 // @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @require     https://raw.githubusercontent.com/jashkenas/underscore/master/underscore.js
-// @require     https://github.com/cbaoth/userscripts/raw/master/lib/cblib.js
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 (function () {
-  // replace amazon order numbers (in bookings) with links to the amazon.de order history search
-  waitForKeyElements(
-    "div.bookingCell__purpose, div.detailsGroup__content > div.text",
-    (e) => {
-      var htmlNew = $(e)
-        .html()
-        .replace(
-          /([0-9]{3}-[0-9]{7}-[0-9]{7})(\s+ama?zo?n)/i,
-          `<a href="https://www.amazon.de/gp/your-account/order-history?opt=ab&search=$1" target="_new">$1</a>$2`
-        );
-      debugger;
-      $(e).html(htmlNew);
-    }
-  );
+  // amazon order number substitution in list view
+  // TODO seems to be a bit glitchy, especially for details view (removed)
+  const AMAZON_ORDER_SEL = "div.bookingCell__purpose"; // div.detailsGroup__content > div.text
+  const AMAZON_REGEX_ORDER =
+    /([a-z0-9][0-9]{2}-[0-9]{7}-[0-9]{7})(\s+(am[az]|kindle))/i;
+  const AMAZON_REGEX_ORDER_SUB = `<a href="https://www.amazon.de/gp/your-account/order-details?orderID=$1" target="_new">$1</a>$2`;
+
+  // replace amazon order numbers (in bookings) with links to the amazon.de order history
+  waitForKeyElements(AMAZON_ORDER_SEL, (e) => {
+    $(e).html($(e).html().replace(AMAZON_REGEX_ORDER, AMAZON_REGEX_ORDER_SUB));
+  });
 })();
