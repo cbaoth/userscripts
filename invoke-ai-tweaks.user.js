8ff02a2f3fe725948b29d73ec1d7f272a3fe2dfa
cbaoth <dev@cbaoth.de>
2023-01-19 18:11:54 +0100

    Update invoke-ai-tweaks             some improvem.

--- invoke-ai-tweaks.user.js
+++ invoke-ai-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Invoke-AI tweaks
 // @description Some tweaks for the invoke-ai web tool
-// @version     0.1
+// @version     0.2
 //
 // @namespace   https://cbaoth.de
 // @author      Andreas Weyer
@@ -30,27 +30,67 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 
     const KEY_B = 66;
     const SEL_INVOKE_BUTTON = `button.invoke-btn[type=submit]`;
-    const SEL_INVOKE_BUTTON_DISABLED = SEL_INVOKE_BUTTON + `[disabled]`;
+    //const SEL_INVOKE_BUTTON_DISABLED = SEL_INVOKE_BUTTON + `[disabled]`;
     const SEL_INVOKE_BUTTON_ENABLED = SEL_INVOKE_BUTTON + `:not([disabled])`;
     const SEL_SAMPLER_SELECT = `div.main-options div:nth-child(3) select`;
+    const SEL_PROMPT = `textarea#prompt`;
     const SAMPLERS = ['ddim', 'plms', 'k_lms', 'k_dpm_2', 'k_dpm_2_a', 'k_dpmpp_2', 'k_dpmpp_2_a', 'k_euler', 'k_euler_a', 'k_heun'];
-    const TIMEOUT_INVOCATION = 20000; // 20sec
+    //const TIMEOUT_INVOCATION = 20000; // 20sec
     const TIMEOUT_INVOCATION_IT = 500; // 500ms between batch iterations
 
-    var batchRunActive = false;
-    var batchRunStartedAt = -1;
-    var batchRunIterationStartedAt = -1;
-    var batchRunSequence = [];
+    let batchRunActive;
+    //let batchRunStartedAt = -1;
+    //let batchRunIterationStartedAt = -1;
+    let batchRunSequence;
+    let batchRunTotal;
+
+
+    // tooltip
+    function showTT(msg, color="orange", size="0.8em") {
+        // use invocation timeout, show tt constantly
+        cb.createTT(msg, TIMEOUT_INVOCATION_IT, { offsetX: 250, offsetY: 25, offsetMouse: false, fadeoutTime: 150, css:{ "font-size": size, "color": color }});
+    }
+
+    function ttAndLog(msg) {
+        showTT(msg);
+        console.log(msg);
+    }
 
     function batchRun() {
         batchRunSequence = []; // clear previous batch run sequence (if any)
-        batchRunStartedAt = Date.now();
-        batchRunIterationStartedAt = -1;
-        //debugger;
-        //SAMPLERS.forEach((sampler, i) => { // loop over all known samplers
-        batchRunSequence = SAMPLERS.filter(s => GM_config.get('iai-sampler-' + s));
+        // batchRunStartedAt = Date.now();
+        // batchRunIterationStartedAt = -1;
+
+        let samplers = SAMPLERS.filter(s => GM_config.get('iai-sampler-' + s));
+        let prompts = GM_config.get('iai-prompt-use') && GM_config.get('iai-prompt-lines').trim().length > 0
+            ? GM_config.get('iai-prompt-lines').split(/\r?\n/)
+            : ['']; // empty string, quick solution to iterate at least once in case no prompts are provided
+
+        prompts.slice().reverse().forEach((p, i) => { // not so efficient but shouldn't matter here
+            samplers.slice().reverse().forEach((s, j) => {
+                batchRunSequence.push([p, s]);
+            });
+        });
+        batchRunTotal = batchRunSequence.length;
         // start iteration
+        batchRunActive = true;
         batchRunIterate();
+    }
+
+    // https://stackoverflow.com/a/59599339
+    // react textArea needs some magic to receive text updates
+    function reactSetInputValue(input, val) {
+        let lastValue = input.value;
+        input.value = val;
+        let event = new Event('input', { bubbles: true });
+        // hack React15
+        event.simulated = true;
+        // hack React16
+        let tracker = input._valueTracker;
+        if (tracker) {
+            tracker.setValue(lastValue);
+        }
+        input.dispatchEvent(event);
     }
 
     // FIXME - make sure that batch runs don't overlap (e.g. pass on an id, stop if id differs)
@@ -58,19 +98,37 @@ this.$ = this.jQuery = jQuery.noConflict(true);
     // TODO - support prompt variable and iteratiion over prompt text (lines in config)
     function batchRunIterate() {
         if (batchRunSequence.length <= 0) {
+            ttAndLog(`Batch Run: Finished!`);
             return; // no further itreations
         }
-        console.log('batch run: still in queue -> ' + batchRunSequence.length);
-        batchRunActive = true
+        if (!batchRunActive) { // stopped?
+            ttAndLog(`Batch Run: Stopped!`);
+            return;
+        }
         // wait 100ms just in case the button is not yet disabled from a potential previous iteration
         if ($(SEL_INVOKE_BUTTON_ENABLED).length >= 1) { // button found and enabled?
-            batchRunIterationStartedAt = Date.now();
-            var sampler = batchRunSequence.pop();
+            //batchRunIterationStartedAt = Date.now();
+            let tuple = batchRunSequence.pop();
+            let idx = batchRunTotal-batchRunSequence.length;
+            let prompt = tuple[0].trim();
+            let sampler = tuple[1];
+            showTT(`Batch Run:  Starting next invocation [${idx}/${batchRunTotal}]`);
+            console.log(`Batch Run: Starting next invocation [${idx}/${batchRunTotal}, sampler: ${sampler}, prompt: ${prompt}]`);
             $(SEL_SAMPLER_SELECT).val(sampler); // select sampler
+            if (GM_config.get('iai-prompt-use')) { // prompt sequence enabled?
+                if (prompt.length <= 0) { // no prompt given (in this line) -> skip
+                    // todo: warning that empty line was skipped
+                    ttAndLog(`Batch Run - skipping empty line!`);
+                    setTimeout(batchRunIterate, TIMEOUT_INVOCATION_IT); // recursion
+                } else {
+                    reactSetInputValue($(SEL_PROMPT)[0], prompt);
+                }
+            }
             $(SEL_INVOKE_BUTTON).click(); // press invoke button
             // todo: random/re-use seed
-            // todo: iterate texts
         } else { // no button or disabled -> wait and retry
+            let idx = batchRunTotal-batchRunSequence.length;
+            ttAndLog(`Batch Run: Invocation [${idx}/${batchRunTotal}] running ...`);
             // todo: timeouts - if (batchRunIterationStartedAt
         }
         setTimeout(batchRunIterate, TIMEOUT_INVOCATION_IT); // recursion
@@ -82,6 +140,8 @@ this.$ = this.jQuery = jQuery.noConflict(true);
         const GM_CONFIG_BATCHRUN_ID = 'InvokeAITweaks_BatchRun_Config'
         const GM_CONFIG_BATCHRUN_FIELDS = {
             'iai-sampler-ddim': {
+                section: ['Samplers',
+                          'Select one or more samplers to be used in sequence'], // Appears above the field
                 type: "checkbox",
                 default: false,
                 label: "ddim"
@@ -130,6 +190,18 @@ this.$ = this.jQuery = jQuery.noConflict(true);
                 type: "checkbox",
                 default: false,
                 label: "k_heun"
+            },
+            'iai-prompt-use': {
+                section: ['Prompts',
+                          'Prompts to be used, one line per invocation (and sampler)'], // Appears above the field
+                type: "checkbox",
+                default: false,
+                label: "Use prompt sequence"
+            },
+            'iai-prompt-lines': {
+                type: "textarea",
+                default: "red apple\ngreen apple\nrotton apple",
+                label: "Prompts"
             }
         }
         GM_config.init({
@@ -139,11 +211,14 @@ this.$ = this.jQuery = jQuery.noConflict(true);
             'events': {
                 'open': function(doc) {
                     batchRunActive = false;
-                    var config = this;
+                    let config = this;
                     doc.getElementById(config.id + '_closeBtn').textContent = 'Cancel';
+                    doc.getElementById(config.id + '_saveBtn').textContent = 'Batch Invoke';
+                    doc.getElementById(config.id + '_field_iai-prompt-lines').cols = 120;
+                    doc.getElementById(config.id + '_field_iai-prompt-lines').rows = 10;
                 },
                 'save': function(values) {
-                    var config = this;
+                    let config = this;
                     config.close();
                     batchRun();
                 }
