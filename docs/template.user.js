708ef53abd068e3d3ecded7209a2d91d2f973312
Andreas Weyer <dev@cbaoth.de>
2026-04-21 20:05:16 +0200

    Update template: prefer concise description in active voice

--- docs/template.user.js
+++ docs/template.user.js
@@ -3,14 +3,14 @@
 // @namespace    https://github.com/cbaoth/userscripts
 // @version      {today's date in YYYY-MM-DD format}
 // @license      MIT
-// @description  {script_description}
+// @description  {concise script description in active voice}
 // @author       cbaoth235
 // @match        https://{website_location}
 // @icon         https://external-content.duckduckgo.com/ip3/{website_domain}.ico
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Your code here...
