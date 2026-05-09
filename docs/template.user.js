76c74157d2494081a2f30740e2446a9006e3c927
Andreas Weyer <dev@cbaoth.de>
2026-05-09 14:39:34 +0200

    Update template: reorder and clarify metadata fields for improved readability

--- docs/template.user.js
+++ docs/template.user.js
@@ -1,15 +1,19 @@
 // ==UserScript==
 // @name         {script_title}
-// @description  {concise_description (in active voice)}
 // @namespace    https://github.com/cbaoth/userscripts
-// @license      MIT
 // @version      {last_modified_at_date (in "YYYY-MM-DD" format)}
+// @description  {concise_description (in active voice)}
 // @author       cbaoth235
-// @exclude      {optional_url_pattern_for_exclusion}
+// @license      MIT
+//
 // @match        {url_pattern_for_inclusion}
-// @require      {optional_url_to_external_library}
+//
 // @grant        {required_permission (else "none")}
-// @icon         https://external-content.duckduckgo.com/ip3/{website_domain}.ico
+//
+// @run-at       {optional_run_at_timing (else "document-end")}
+//
+// @require      {optional_url_to_external_library}
+// @icon         {optional_url_to_icon (https://external-content.duckduckgo.com/ip3/{website_domain}.ico)}
 // @downloadURL  https://github.com/cbaoth/userscripts/raw/master/{script_filename}
 // @updateURL    https://github.com/cbaoth/userscripts/raw/master/{script_filename}
 // ==/UserScript==
